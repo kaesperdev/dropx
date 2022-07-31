8563bdda36c34ec7de9e325cbb2ca226949732ec
fvnnydev <mail@fvnny.dev>
2022-07-31 12:16:28 +0200

    refactor: remove console.log

--- src/utils/sizeLimiter.js
+++ src/utils/sizeLimiter.js
@@ -1,8 +1,6 @@
 const maxImageSize = process.env.MAX_IMAGE_SIZE.replace('MB', '') || Infinity;
 const maxPasteSize = process.env.MAX_PASTE_SIZE.replace('MB', '') || Infinity;
 
-console.log(maxImageSize, maxPasteSize);
-
 const checkUploadSize = (size, uploadType) => {
   if (uploadType === 'image') {
     return size / 1024 / 1024 <= maxImageSize;
