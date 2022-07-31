591ccb512ac6f0853b699bc35dae0b41aafe0686
fvnnydev <mail@fvnny.dev>
2022-07-31 13:46:50 +0200

    fix: fix input processing in size limiter

--- src/utils/sizeLimiter.js
+++ src/utils/sizeLimiter.js
@@ -1,11 +1,11 @@
-const maxImageSize = process.env.MAX_IMAGE_SIZE.replace('MB', '') || Infinity;
-const maxPasteSize = process.env.MAX_PASTE_SIZE.replace('MB', '') || Infinity;
+const maxImageSize = process.env.MAX_IMAGE_SIZE || Infinity;
+const maxPasteSize = process.env.MAX_PASTE_SIZE || Infinity;
 
 const checkUploadSize = (size, uploadType) => {
   if (uploadType === 'image') {
-    return size / 1024 / 1024 <= maxImageSize;
+    return size / 1024 / 1024 <= maxImageSize.replace('MB', '');
   } else if (uploadType === 'paste') {
-    return size / 1024 / 1024 <= maxPasteSize;
+    return size / 1024 / 1024 <= maxPasteSize.replace('MB', '');
   }
 };
 
